f34f2a65ae3a47a35bf830800ecb2e7ea8628dd7
Taketora <109759654+taketora-dev@users.noreply.github.com>
2025-08-19 16:12:25 +0700

    Update config.lua

--- config.lua
+++ config.lua
@@ -1,11 +1,12 @@
 Config = {}
 
 -- Planning to add other targeting options in a future version, leave this for now
-Config.TargetingSystem = 'qb-target'
+Config.TargetingSystem = 'ox_target'
 
 -- Set the amount of stress reduced per action
-Config.StressRemoval = 2
+Config.StressRemoval = 10
 
 function GetStressRemovalAmount()
     return Config.StressRemoval
+
 end
